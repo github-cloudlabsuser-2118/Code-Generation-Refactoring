b8293b153ee8d7c6fea3cce64a22c2201017e15b
github-cloudlabsuser-2118 <169144456+github-cloudlabsuser-2118@users.noreply.github.com>
2024-11-20 16:32:37 +0000

    cambio de ciudad

--- consulta_api.mjs
+++ consulta_api.mjs
@@ -1,7 +1,7 @@
 import https from 'https';
 
 const apiKey = '0440102188bbf10f1f811e93bbb0d3da'; // Replace with your OpenWeatherMap API key
-const city = 'London'; // Replace with the city you want to get the weather for
+const city = 'Oxford'; // Replace with the city you want to get the weather for
 
 const getWeather = (city) => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
